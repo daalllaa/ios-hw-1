df3b1aa1e3dcfe6ef0dfb372e31c031862d50815
daallaa <dalal.alj003@GMAIL.COM>
2020-06-16 07:20:23 +0300

    تم ✅

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,9 +5,9 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
-name = "Salem"
+name = "Ahmad"
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = "myWeight+FriendWeight"
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = "myWeight(height * height)"
 
 /*
 - - - -
@@ -38,36 +38,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
+var firstName1 = "bedor"
+var lastName1 = "aljohar"
+var email1 = ".com"
+var phoneNumber1 = "1234556"
+var age1 = 33
+var country1 = "kuwait"
+var area1 = "alf"
+var password1 = "123456"
 var isKuwaiti = false
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "samera"
+var lastName2 = "ra"
+var email2 = ".com"
+var phoneNumber2 = "11223344"
+var age2 = "22"
+var  country2 = "norway"
+var area2 = "loften"
+var password2 = "112243"
+var IsKuwaiti = false
 
 //Person 3
-
-
-
-
-
-
-
-
+var firstName3 = "lulu"
+var lastName3 = "lulua"
+var email3 = ".com "
+var phoneNumber3 =  "12121"
+var age3 = 16
+var country3 = "croatia"
+var area3 = "zabar"
+var Password3 = "1234438721"
+var LsKuwaiti = false
 
 /*
 - - - -
@@ -82,3 +83,23 @@ var isKuwaiti = false
 
  
  */
+firstName1 != firstName2
+ lastName1 != lastName2
+ email1 !=  email2
+ phoneNumber1  != phoneNumber2
+ country1 !=  country2
+area1  != area2
+ password1 = password2
+
+firstName3 != firstName1
+ lastName3 != lastName1
+ email3 !=  email1
+ phoneNumber3  !=  phoneNumber1
+ country3 !=  country2
+ area3 =  area2
+ Password3 = password2
+
+age1 > 18
+age3 < 18
+age3 != 17
+
